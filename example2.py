24ce3abf1b10080f1e704ba893f49f88aecdf71e
Yana <jana.lebedeva@ro.ru>
2018-06-01 02:11:52 +0700

    We added file

---
+++ example2.py
@@ -0,0 +1,32 @@
+import random
+
+
+class Example_right:
+
+    def __init__(self):
+        self._value = 25
+
+    def ex(self, x):
+        function = self.value * xrange
+        print("x=", x, "Результат=", function)
+
+
+class Iterator:
+
+    def __init__(self):
+        self.x = 0
+
+        def randomize(self):
+            self.x = random.randint(0, 25)
+
+        def main():
+            iterator = Iterator()
+            print(iterator.x)
+            iterator.randomize()
+            print(iterator.x)
+            example = Example_right()
+            example.ex(iterator.x)
+
+
+if __name__ == '__main__':
+    main()
